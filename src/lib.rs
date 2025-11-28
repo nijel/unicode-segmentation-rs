bee389d9251584ad15c3b7aac9af3fe8359a8802
Michal Čihař <michal@cihar.com>
2025-11-28 08:33:16 +0100

    feat: add type annotations
    Add type annotations and clarify some docstrings.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod unicode_segmentation_rs {
     use unicode_segmentation::UnicodeSegmentation;
     use unicode_width::UnicodeWidthStr;
 
-    /// Split a string into grapheme clusters
+    /// Split a string into grapheme clusters.
     #[pyfunction]
     fn graphemes(text: &str, is_extended: bool) -> PyResult<Vec<String>> {
         Ok(text.graphemes(is_extended).map(|s| s.to_string()).collect())
@@ -23,13 +23,13 @@ mod unicode_segmentation_rs {
             .collect())
     }
 
-    /// Split a string into words
+    /// Split a string at word boundaries (includes punctuation and whitespace).
     #[pyfunction]
     fn split_word_bounds(text: &str) -> PyResult<Vec<String>> {
         Ok(text.split_word_bounds().map(|s| s.to_string()).collect())
     }
 
-    /// Split a string into word indices
+    /// Split a string at word boundaries with indices.
     #[pyfunction]
     fn split_word_bound_indices(text: &str) -> PyResult<Vec<(usize, String)>> {
         Ok(text
@@ -38,13 +38,13 @@ mod unicode_segmentation_rs {
             .collect())
     }
 
-    /// Get Unicode words (excluding punctuation and whitespace)
+    /// Get Unicode words from a string (excludes punctuation and whitespace).
     #[pyfunction]
     fn unicode_words(text: &str) -> PyResult<Vec<String>> {
         Ok(text.unicode_words().map(|s| s.to_string()).collect())
     }
 
-    /// Split a string into sentences
+    /// Split a string at word boundaries (includes punctuation and whitespace).
     #[pyfunction]
     fn unicode_sentences(text: &str) -> PyResult<Vec<String>> {
         Ok(text.unicode_sentences().map(|s| s.to_string()).collect())
